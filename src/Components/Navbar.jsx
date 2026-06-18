620570a2dad16f734985638469abc7ce432177a2
Jatto4199 <jattobatholomew@gmail.com>
2026-06-18 20:52:57 +0100

    chore: clear git cache and fix strict file casing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import myLogo from "../assets/myLogo.png";
+import myLogo from "../assets/MyLogo.png";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
